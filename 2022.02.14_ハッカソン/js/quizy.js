19e7ad5848dc9d128eee7e389e9a57fb0b28f620
Rei Tanoue <tanouser463@gmail.com>
2022-02-15 22:24:37 +0900

    ポップアップの背景色を変更し、遷移先の前ページに配置した

--- 2022.02.14_ハッカソン/js/quizy.js
+++ 2022.02.14_ハッカソン/js/quizy.js
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 "use strict";
 //選択肢配列
 let questionsText = [
@@ -31,8 +25,6 @@ let explanationText = [
   "2.0期生は去年の6月に、2.5期生は去年の11月にPOSSEに入会してくれました！",
   "POSSE2では、ハーバーズに宿泊して朝まで色々な話をします！今後の人生相談や、恋バナなんかも！？",
 ];
-
-
 
 //画像配列
 let images = [
@@ -116,6 +108,9 @@ function show_answer_correct(quiz_num) {
 
   let optionList = document.getElementById(`optionList${quiz_num}`);
   optionList.style.pointerEvents = "none";
+  document
+    .getElementById(`optionList${quiz_num}`)
+    .scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
 }
 //1つ目の不正解のボタン押したときの表示変更や回答ボックスの表示
 function show_answer_wrong_1(quiz_num) {
@@ -129,6 +124,9 @@ function show_answer_wrong_1(quiz_num) {
 
   let optionList = document.getElementById(`optionList${quiz_num}`);
   optionList.style.pointerEvents = "none";
+  document
+    .getElementById(`optionList${quiz_num}`)
+    .scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
 }
 //2つ目の不正解のボタン押したときの表示変更や回答ボックスの表示
 function show_answer_wrong_2(quiz_num) {
@@ -142,71 +140,7 @@ function show_answer_wrong_2(quiz_num) {
 
   let optionList = document.getElementById(`optionList${quiz_num}`);
   optionList.style.pointerEvents = "none";
-//本体
-  for (let i = 0; i < quizSet.length; i++) {
-    let question_box =
-      `<h1 class="question-text">` +
-      `${i + 1}.${questionsText[i]}` +
-      `</h1>` +
-      `<div class="quiz-image-container">` +
-      `<img class="quiz-image" src="` +
-      images[i] +
-      `" alt="問題の写真">` +
-      `</div>` +
-      `<div class="box1">` +
-      `<ul id="ul-${i}" >`;
-    for (let x = 0; x < 3; x++) {
-      question_box = question_box +
-        `<li id='${i}-${x}' onclick="onclickFunction(${i},${x})" >` + quizSet[i][x] + `</li>`
-    };
-    question_box =
-      question_box +
-      `</ul>` +
-      `</div>` +
-      `<div class="quiz-result" id="ans-t-${i}">` +
-      `<p class ="quiz-result-title quiz-result-title-succeeded">正解！</p>` +
-      `<p class ="quiz-result-explanation">正解は「` +
-      true_answers[i] +
-      `」です！<br>${explanationText[i]}</p>` +
-      `</div>` +
-      `<div class="quiz-result" id="ans-f-${i}">` +
-      `<p class ="quiz-result-title quiz-result-title-failed">不正解！</p>` +
-      `<p class ="quiz-result-explanation">正解は「` +
-      true_answers[i] +
-      `」です！<br>${explanationText[i]}</p>` +
-      `</div>`;
-    //html内に入れる
-    document.getElementById('quiz-box').insertAdjacentHTML('beforeend', question_box);
-
-    var onclickFunction = (question_number, clicked_number) => {
-      //クリックした際に全て赤に変更
-      let clicked_option = document.getElementById(question_number + "-" + clicked_number)
-      clicked_option.classList.add("failed");
-      //正解のボックスが青になるようにする（中身で判断）
-      for (let p = 0; p < 3; p++) {
-        if (quizSet[question_number][p] == true_answers[question_number]) {
-          let true_choice = document.getElementById(question_number + "-" + p);
-          true_choice.classList.add("succeeded");
-          true_choice.classList.remove("failed");//先ほどの「クリックした際に全て赤に変更」を取り除く
-        }
-      }
-      //全ての選択肢のクリック無効
-      let choice0 = document.getElementById(question_number + "-0");
-      let choice1 = document.getElementById(question_number + "-1");
-      let choice2 = document.getElementById(question_number + "-2");
-      choice0.style.pointerEvents = "none";
-      choice1.style.pointerEvents = "none";
-      choice2.style.pointerEvents = "none";
-      //正解を選択したときの解説ブロックを表示
-      if (quizSet[question_number][clicked_number] == true_answers[question_number]) {
-        document.getElementById('ans-t-' + question_number).style.display = "block";
-        document.getElementById('ul-' + question_number).scrollIntoView({ behavior: 'smooth', block: 'start' });
-        //不正解を選択したときの解説ブロックを表示
-      } else {
-        document.getElementById('ans-f-' + question_number).style.display = "block";
-        document.getElementById('ul-' + question_number).scrollIntoView({ behavior: 'smooth', block: 'start' });
-      }
-    }
-  }
-// console.log('a')
+  document
+    .getElementById(`optionList${quiz_num}`)
+    .scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
 }
