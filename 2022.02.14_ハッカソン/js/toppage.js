4ade810232ceb1348f04b7874fc50d8df622b3ff
Rei Tanoue <tanouser463@gmail.com>
2022-02-16 10:11:02 +0900

    新しいブランチを切るので

--- 2022.02.14_ハッカソン/js/toppage.js
+++ 2022.02.14_ハッカソン/js/toppage.js
@@ -41,3 +41,4 @@ typing('#typing1', 'P O S S E 2 の 基 本 情 報');
 typing('#typing2', 'P O S S E 2 の 魅 力 と は ？');
 typing('#typing3', 'ここが  違 う！！P O S S E 2 な ら で は の 特 徴');
 typing('#typing4', '2.5 期 生 P O S S E 2 の メ ン バ ー か ら ひ と  言');
+
